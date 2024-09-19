b51417f22f6794d0948ca3fe26245ff96b01407c
jotave <jvfelix86@gmail.com>
2024-09-19 19:14:25 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+
+    slideIndex += n;
+
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
+
+  document.getElementById('contatos-icon').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    
+    
+    document.querySelector('#contatos').scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+
+  
+
+  
